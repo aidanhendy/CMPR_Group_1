c879d18b42d75c530914ce465431f811a4c3688f
aidanhendy <aidan.henderson214@gmail.com>
2025-10-21 13:05:21 -0700

    23

--- Participant.h
+++ Participant.h
@@ -3,16 +3,35 @@
 #include <fstream>
 using namespace std;
 
+
 class Participant {
 private:
-    int milage;
+
+    struct runs
+    {
+        string neighborhood;
+        double distance;
+    };
+    
+
+    double milage;
     string name;
     int number;
+    
 public:
-    Participant(int mile, string name, int num);
-    void displayInfo();
+
+    
+    Participant(double mile, string name, int num);
+
+    //big 3
+    ~Participant();
+    Participant(); //copy constructor
+    Participant& operator=(const Participant& par); // overloaded = operator
+
+    //everything else
     static void readFile(string file); //read function
     static string bestRunner(string file); //function to find best runner
+    void displayInfo();
 };
 
 
