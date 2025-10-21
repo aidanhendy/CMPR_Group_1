29281954afa602fdd36744dfc2b4a18346983bfa
aidanhendy <144183312+aidanhendy@users.noreply.github.com>
2025-10-21 12:51:33 -0700

    Add Participant class with file handling methods

--- Participant.h
+++ Participant.h
@@ -1,1 +1,16 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Participant {
+private:
+    int milage;
+    string name;
+    int number;
+public:
+    Participant(int mile, string name, int num);
+    void displayInfo();
+    static void readFile(string file); //read function
+    static string bestRunner(string file); //function to find best runner
+};
