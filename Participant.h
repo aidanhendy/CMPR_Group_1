973e60471fc54042d8f273509aa0af796b30a2ff
aidanhendy <aidan.henderson214@gmail.com>
2025-10-21 12:36:02 -0700

    1

--- Participant.h
+++ Participant.h
@@ -3,9 +3,15 @@
 #include <fstream>
 using namespace std;
 
+class Participant {
 private:
     int milage;
     string name;
     int number;
 public:
-    
+    Participant(int, string, int);
+    void displayInfo();
+    string bestRunner();
+};
+
+
