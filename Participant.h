3f28d1092a145a58f67bd0ec3c8217baebff01aa
aidanhendy <144183312+aidanhendy@users.noreply.github.com>
2025-10-21 14:34:28 -0700

    Update Participant class with runs management
    Refactor Participant class to include runs structure and manage multiple entries.

--- Participant.h
+++ Participant.h
@@ -3,14 +3,42 @@
 #include <fstream>
 using namespace std;
 
+
 class Participant {
 private:
-    int milage;
+
+    struct runs
+    {
+        string neighborhood;
+        double distance;
+    };
+    
+
+    double totalMilage;
     string name;
     int number;
+    runs* runsArray;
+    const int CAP = 6; //for array
+    int noe; //number of entries
+    
+
+    
 public:
-    Participant(int mile, string name, int num);
-    void displayInfo();
+
+    
+    Participant();
+
+    //big 3
+    ~Participant();
+    Participant(); //copy constructor
+    Participant& operator=(const Participant& par); // overloaded = operator
+
+    //everything else
+    static int numOfParticipants(string file); //Counts number of participants.
     static void readFile(string file); //read function
+    static void readLine(string line); // read individual line
     static string bestRunner(string file); //function to find best runner
+    void displayInfo();
 };
+
+
