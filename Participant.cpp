cb1646f65da2d988495a7e37bd7bb30e0cf53191
aidanhendy <144183312+aidanhendy@users.noreply.github.com>
2025-10-23 12:42:13 -0700

    Update Participant.cpp

--- Participant.cpp
+++ Participant.cpp
@@ -2,66 +2,57 @@
 #include <fstream>
 #include <string>
 
-int Participant:: numOfParticipants(string file)
-{
-    ifstream inFile;
-    inFile.open("participantData.txt");
-    string line;
-    int count;
-
-while(getline(inFile, line))
-{
-    count++;
+Participant::Participant() {
+    number = 0;
+    name = "";
+    totalMileage = 0.0;
+    for (int i = 0; i < 6; i++)
+        runsArray[i].distance = 0.0;
 }
 
-}
-
-
-
-
-
-
-void Participant:: readFile(string file)
-{
-    ifstream inFile;
-    inFile.open("participantData.txt");
-    string line;
-
-    if(!inFile)
-    {
-        cerr << "Error opening file!" << endl;
-        return;
+Participant::Participant(int num, string name, double distances[6]) {
+    number = num;
+    name = name;
+    totalMileage = 0.0;
+    for (int i = 0; i < 6; i++) {
+        runsArray[i].distance = distances[i];
+        totalMileage += distances[i];
     }
-
-    while(getline(inFile, line))
-    {
-
-    }
-
-
-while()
-{
-
-current++;
-
-
 }
 
-//while(infile >> num &&)
-
-
-
-
+double Participant::getTotalMileage() const {
+    return totalMileage;
 }
-void Participant:: readLine(string line)
-{
-    int temp;
-    for(int i =0; i < line.size();i++)
+
+string Participant::getName() const {
+    return name;
+}
+
+int Participant::getNumber() const {
+    return number;
+}
+
+int Participant::numOfParticipants(const string file) {
+    ifstream inFile(file);
+    string line;
+    int count = 0;
+    while (getline(inFile, line))
     {
-        if (i ==' ' && i >= 30 && i <= 39)
+        if (!line.empty()) 
         {
-          temp = stoi(line.substr(0, i -1));
+            count++;
         }
-    }
-    
+    } 
+    inFile.close();
+    return count;
+}
+
+void Participant::readFile(const string& file, Participant participants[], int& count)
+{
+
+}
+
+string bestRunner(const Participant participants[], int count)
+{
+
 }
